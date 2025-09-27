cce2f9673c0b2ce65a6750d70a3213ef4f5bff9c
samerayari <s235677@dtu.dk>
2025-09-27 17:31:38 +0200

    Aflevering 4

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SparePartsInventoryAssistant
+{
+    static void Main(string[] args)
+    {
+        List<string> inventory = new List<string>()
+        {
+            "hydraulic pump",
+            "PLC module",
+            "servo motor"
+        };
+
+        Console.WriteLine("Hej. Welcome to the spare parts inventory!");
+
+        while (true)
+        {
+            Console.Write("Which part do you need? ");
+            string part = Console.ReadLine();
+            
+            if (part.Equals("Do you actually have any parts?", StringComparison.OrdinalIgnoreCase)
+                || part.Equals("Is there anything in stock at all?", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"We have {inventory.Count} part(s)!");
+                foreach (var p in inventory)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+           
+            else if (inventory.Contains(part))
+            {
+                Console.WriteLine($"I've got {part} here for you 😊. Bye!");
+                break; 
+            }
+            else
+            {
+                Console.WriteLine($"I am afraid we don't have any {part} in the inventory 😔");
+            }
+        }
+    }
+}
